1e4ecee20b07352900edbcf1a274b62a8a5eb1e8
hariyanuar <hariyanuar.hy1@gmail.com>
2021-02-27 10:00:42 +0700

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/absendigital/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.absendigital
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main () => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Hello World!')),
+      ),
+    );
+  }
+}
